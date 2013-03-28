816909bf87600a5367c7ee8e7b15c60016a0b833
Nick Sagona <nick@moc10media.com>
2013-03-27 22:33:38 -0500

    Add check methods to the Phire\Project class

--- phirecms2/phire-cms/module/Phire/src/Phire/Project.php
+++ phirecms2/phire-cms/module/Phire/src/Phire/Project.php
@@ -44,5 +44,81 @@ class Project extends P
         parent::run();
     }
 
+    /**
+     * Method to check if the system is installed
+     *
+     * @throws \Exception
+     * @return boolean
+     */
+    public static function isInstalled()
+    {
+        if (($_SERVER['REQUEST_URI'] != BASE_PATH . APP_URI . '/install') &&
+            ($_SERVER['REQUEST_URI'] != BASE_PATH . APP_URI . '/install/user') &&
+            ((DB_INTERFACE == '') || (DB_NAME == ''))) {
+            throw new \Exception('Error: The config file is not properly configured. Please check the config file or install the application.');
+        }
+
+        return true;
+    }
+
+    /**
+     * Determine whether or not the necessary system directories are writable or not.
+     *
+     * @param  string $contentDir
+     * @param  boolean $msgs
+     * @return boolean|array
+     */
+    public static function checkDirs($contentDir, $msgs = false)
+    {
+        $dir = new \Pop\File\Dir($contentDir, true, true);
+        $files = $dir->getFiles();
+        $errorMsgs = array();
+
+        // Check if the necessary directories are writable for Windows.
+        if (stripos(PHP_OS, 'win') !== false) {
+            touch($contentDir . '/writetest.txt');
+            clearstatcache();
+            if (!file_exists($contentDir . '/writetest.txt')) {
+                $errorMsgs[] = "The directory " . str_replace($_SERVER['DOCUMENT_ROOT'], '', $contentDir) . " is not writable.";
+            } else {
+                unlink($contentDir . '/writetest.txt');
+            }
+            foreach ($files as $value) {
+                if (is_dir($value)) {
+                    touch($value . '/writetest.txt');
+                    clearstatcache();
+                    if (!file_exists($value . '/writetest.txt')) {
+                        $errorMsgs[] = "The directory " . str_replace($_SERVER['DOCUMENT_ROOT'], '', $value) . " is not writable.";
+                    } else {
+                        unlink($value . '/writetest.txt');
+                    }
+                }
+            }
+            // Check if the necessary directories are writable for Unix/Linux.
+        } else {
+            clearstatcache();
+            if (!is_writable($contentDir)) {
+                $errorMsgs[] = "The directory " . str_replace($_SERVER['DOCUMENT_ROOT'], '', $contentDir) . " is not writable.";
+            }
+            foreach ($files as $value) {
+                if (is_dir($value)) {
+                    clearstatcache();
+                    if (!is_writable($value)) {
+                        $errorMsgs[] = "The directory " . str_replace($_SERVER['DOCUMENT_ROOT'], '', $value) . " is not writable.";
+                    }
+                }
+            }
+        }
+
+        // If the messaging flag was passed, return any
+        // error messages, else return true/false.
+        if ($msgs) {
+            return $errorMsgs;
+        } else {
+            return (count($errorMsgs) == 0) ? true : false;
+        }
+
+    }
+
 }
 
