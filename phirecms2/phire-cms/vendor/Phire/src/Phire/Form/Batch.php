72d252ca80d1139412936a454b0576c12029e129
Nick Sagona <nick@moc10media.com>
2013-09-16 12:36:48 -0500

    Finalize the batch upload process

--- phirecms2/phire-cms/vendor/Phire/src/Phire/Form/Batch.php
+++ phirecms2/phire-cms/vendor/Phire/src/Phire/Form/Batch.php
@@ -20,27 +20,38 @@ class Batch extends Form
      */
     public function __construct($action = null, $method = 'post', $tid = 0)
     {
-        $this->initFieldsValues = array(
+        $fields1 = array(
             'file_name_1' => array(
                 'type'       => 'file',
-                'label'      => '<a href="#" onclick="addBatchFields(); return false;">[+]</a> Single File / Title:',
+                'label'      => '<a href="#" onclick="addBatchFields(); return false;">[+]</a> File / Title:',
                 'attributes' => array(
                     'size' => 30,
-                    'style' => 'display: block;'
+                    'style' => 'display: block; margin: 0 0 5px 0; padding: 0 0 5px 0;'
                 )
             ),
             'file_title_1' => array(
                 'type'       => 'text',
                 'attributes' => array(
                     'size' => 50,
-                    'style' => 'display: block;'
+                    'style' => 'display: block; margin: 0 0 5px 0; padding: 0 0 5px 0;'
                 )
-            ),
-            'archive_file' => array(
-                'type'       => 'file',
-                'label'      => 'Archive File (Multiple Files):<br /><span style="display: block; margin: 1px 0 0 0; font-size: 0.9em;"><strong>Supported Archive Types</strong><br />' . implode(', ', array_keys(\Pop\Archive\Archive::formats())) . '</span>',
-                'attributes' => array('size' => 30)
-            ),
+            )
+        );
+
+        $fields2 = array();
+        $formats = \Pop\Archive\Archive::formats();
+
+        if (count($formats) > 0) {
+            $fields2 = array(
+                'archive_file' => array(
+                    'type'       => 'file',
+                    'label'      => 'Archive of Multiple Files:<br /><span style="display: block; margin: 5px 0 0 0; font-size: 0.9em;"><strong>Supported Archive Types</strong><br />' . implode(', ', array_keys($formats)) . '</span>',
+                    'attributes' => array('size' => 30)
+                )
+            );
+        }
+
+        $fields3 = array(
             'type_id' => array(
                 'type'  => 'hidden',
                 'value' => $tid
@@ -52,6 +63,7 @@ class Batch extends Form
             )
         );
 
+        $this->initFieldsValues = array($fields1, $fields2, $fields3);
         parent::__construct($action, $method, null, '    ');
         $this->setAttributes('id', 'batch-form');
     }
